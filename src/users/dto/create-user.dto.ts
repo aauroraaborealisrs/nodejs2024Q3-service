5ae1e7c2290be9c6f974f55d80a604f1b3b592f2
aauroraaborealisrs <imaginepeach@mail.ru>
2024-11-09 20:32:47 +0300

    docs: add demo docs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,19 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User login',
+    example: 'TestUser',
+  })
   @IsString()
   @IsNotEmpty()
   login: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'TestPassword',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
--- src/users/dto/update-password.dto.ts
+++ src/users/dto/update-password.dto.ts
@@ -1,10 +1,19 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePasswordDto {
+  @ApiProperty({
+    description: 'Old password of the user',
+    example: 'OldPassword123',
+  })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
 
+  @ApiProperty({
+    description: 'New password of the user',
+    example: 'NewPassword123',
+  })
   @IsString()
   @IsNotEmpty()
   newPassword: string;
