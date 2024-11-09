82511777f884e8f19fa8a374c9436475ff08ebc7
aauroraaborealisrs <imaginepeach@mail.ru>
2024-11-09 14:57:58 +0300

    init: start task

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Server is running on http://localhost:${port}`);
 }
+
 bootstrap();
