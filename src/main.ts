f8711bbc3f451bd6fecfd43fc29a3ab6dbeb4655
aauroraaborealisrs <imaginepeach@mail.ru>
2024-11-09 16:00:30 +0300

    feat: add user

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 4000;
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   console.log(`Server is running on http://localhost:${port}`);
 }
